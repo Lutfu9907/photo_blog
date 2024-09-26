3c823e694c3cb941381b1f97fbad4da125791b49
LUTFU <dolaylutfu@gmail.com>
2024-09-26 19:13:22 +0300

    ejs uploaded

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
+//template engine -->ejs(embedded javaScript)
+app.set('view engine', 'ejs');
+
 //middleware req-res arasında bulunan durumlar.
 app.use(express.static('public'));
 
+//Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 const port = 3000;
